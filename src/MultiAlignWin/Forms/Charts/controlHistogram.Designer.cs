69ffa3bfee31ca725ce19e1938ff8561a8f0f27c
Brian LaMarche <brian.lamarche@pnnl.gov>
2010-08-13 01:00:26 +0000

    Some refactoring was causing this not to build in hudson.  SVN folder was recursively copied by VS.  Fixing .

---
+++ src/MultiAlignWin/Forms/Charts/controlHistogram.Designer.cs
@@ -0,0 +1,79 @@
+namespace MultiAlignWin.Drawing
+{
+    partial class controlHistogram
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            PNNLControls.PenProvider penProvider1 = new PNNLControls.PenProvider();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // controlHistogram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ChartLayout.LegendFraction = 0.2F;
+            this.ChartLayout.LegendLocation = PNNLControls.ChartLegendLocation.Right;
+            this.ChartLayout.MaxLegendHeight = 150;
+            this.ChartLayout.MaxLegendWidth = 250;
+            this.ChartLayout.MaxTitleHeight = 50;
+            this.ChartLayout.MinLegendHeight = 50;
+            this.ChartLayout.MinLegendWidth = 75;
+            this.ChartLayout.MinTitleHeight = 15;
+            this.ChartLayout.TitleFraction = 0.1F;
+            this.DefaultZoomHandler.Active = true;
+            this.DefaultZoomHandler.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(119)))), ((int)(((byte)(136)))), ((int)(((byte)(153)))));
+            this.DefaultZoomHandler.LineColor = System.Drawing.Color.Black;
+            this.Legend.BackColor = System.Drawing.Color.Transparent;
+            penProvider1.Color = System.Drawing.Color.Black;
+            penProvider1.Width = 1F;
+            this.Legend.BorderPen = penProvider1;
+            this.Legend.Bounds = new System.Drawing.Rectangle(533, 80, 127, 395);
+            this.Legend.ColumnWidth = 125;
+            this.Legend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Legend.MaxFontSize = 12F;
+            this.Legend.MinFontSize = 6F;
+            this.Margins.BottomMarginFraction = 0.1F;
+            this.Margins.BottomMarginMax = 72;
+            this.Margins.BottomMarginMin = 30;
+            this.Margins.DefaultMarginFraction = 0.05F;
+            this.Margins.DefaultMarginMax = 15;
+            this.Margins.DefaultMarginMin = 5;
+            this.Margins.LeftMarginFraction = 0.2F;
+            this.Margins.LeftMarginMax = 150;
+            this.Margins.LeftMarginMin = 72;
+            this.Name = "controlHistogram";
+            this.Size = new System.Drawing.Size(666, 520);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+    }
+}
