87c57b848787ba4b87e028749b7dce261094323e
Brian LaMarche <brian.lamarche@pnnl.gov>
2011-12-21 22:52:58 +0000

    Moved the abundance reporting type into its own enumeration.

---
+++ src/MultiAlignCore/Algorithms/FeatureFinding/AbundanceReportingType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiAlignCore.Algorithms.FeatureFinding
+{
+    /// <summary>
+    /// The way to report abundance.
+    /// </summary>
+    public enum AbundanceReportingType
+    {
+        Sum,
+        Max
+    }
+}
