394b29fb971eab10e28d21eeb4f6aa2f534d3026
Brian LaMarche <brian.lamarche@pnnl.gov>
2011-12-21 21:16:30 +0000

    Parameter file editing.

--- src/MultiAlignCore/IO/Parameters/ParameterFileAttribute.cs
+++ src/MultiAlignCore/IO/Parameters/ParameterFileAttribute.cs
@@ -5,33 +5,33 @@ using System.Text;
 
 namespace MultiAlignCore.IO.Parameters
 {
+    /// <summary>
+    /// Attribute class for persisting primitive type properties to a parameter file.
+    /// </summary>
     [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
     public class ParameterFileAttribute : Attribute
     {
-
-        public ParameterFileAttribute(string name, string groupName, string fullDescription)
-        {
-            Name = name;
-            GroupName = groupName;
-            FullDecription = fullDescription;
-        }
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="groupName"></param>
         public ParameterFileAttribute(string name, string groupName)
         {
-            Name     = name;
-            GroupName       = groupName;
-            FullDecription  = "";
+            Name            = name;
+            GroupName       = groupName;            
         }
-        
-        public string FullDecription 
-        { 
-            get; 
-            private set; 
-        }
+        /// <summary>
+        /// Gets the name of the Parameter
+        /// </summary>
         public string Name 
         { 
             get; 
             private set; 
         }
+        /// <summary>
+        /// Gets the group the parameter belongs to.
+        /// </summary>
         public string GroupName 
         { 
             get; 
---
+++ src/MultiAlignCore/IO/Parameters/ParameterFileGroupAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MultiAlignCore.IO.Parameters
+{
+    /// <summary>
+    /// Attribute to tag an options class for parameter file persistence.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    public class ParameterFileGroupAttribute : Attribute
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="name">Name of the group.</param>
+        /// <param name="fullDescription">Description of the group.</param>
+        public ParameterFileGroupAttribute(string name, string fullDescription)
+        {
+            Name            = name;
+            FullDecription  = fullDescription;
+        }
+        /// <summary>
+        /// Gets the description of the group.
+        /// </summary>
+        public string FullDecription
+        {
+            get;
+            private set;
+        }
+        /// <summary>
+        /// Gets the name of the group.
+        /// </summary>
+        public string Name
+        {
+            get;
+            private set;
+        }
+    }
+}
