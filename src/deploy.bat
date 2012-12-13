ce7aa35ed9a5bcb7b4efd896c109f1bbe966a278
Brian LaMarche <brian.lamarche@pnnl.gov>
2012-12-13 05:53:35 +0000

    Fixed the deploy script

--- src/deploy.bat
+++ src/deploy.bat
@@ -12,7 +12,7 @@ SET "version=%major%.%minor%.%build%"
 SET "vDir=MultiAlign-v"
 SET "finalVersion=%vDir%%version%"
 SET "outpath=\\floyd\software\MultiAlign\test\%finalVersion%"
-SET "localpath=m:\software\proteomics\MultiAlign\test\%finalVersion%"
+SET "localpath=m:\software\proteomics\MultiAlign\deployed\%finalVersion%"
 
 ECHO %outpath%
 
@@ -26,26 +26,26 @@ mkdir %x86%
 mkdir %x64%
 
 mkdir %x86%\MultiAlignParameterFileEditor
-mkdir %x86%\Manassa
+mkdir %x86%\MultiAlign
 mkdir %x86%\MultiAlignConsole
 
 mkdir %x64%\MultiAlignParameterFileEditor
-mkdir %x64%\Manassa
+mkdir %x64%\MultiAlign
 mkdir %x64%\MultiAlignConsole
 
 xcopy /S .\MultiAlignParameterFileEditor\bin\x86\release\*  	%x86%\MultiAlignParameterFileEditor
-xcopy /S .\Manassa\bin\x86\release\*  				%x86%\Manassa
+xcopy /S .\Manassa\bin\x86\release\*  				%x86%\MultiAlign
 xcopy /S .\MultiAlignConsole\bin\x86\release\*  		%x86%\MultiAlignConsole
 
 xcopy /S .\MultiAlignParameterFileEditor\bin\x64\release\*  	%x64%\MultiAlignParameterFileEditor
-xcopy /S .\Manassa\bin\x64\release\*  				%x64%\Manassa
+xcopy /S .\Manassa\bin\x64\release\*  				%x64%\MultiAlign
 xcopy /S .\MultiAlignConsole\bin\x64\release\*  		%x64%\MultiAlignConsole
 
 ECHO Cleaning up directories
 del /F /S %x86%\MultiAlignParameterFileEditor\*.vshost.exe*
 del /F /S %x86%\MultiAlignParameterFileEditor\*.pdb
-del /F /S %x86%\Manassa\*.vshost.exe*
-del /F /S %x86%\Manassa\*.pdb
+del /F /S %x86%\MultiAlign\*.vshost.exe*
+del /F /S %x86%\MultiAlign\*.pdb
 
 rmdir /Q /S %x86%\MultiAlignConsole\examples
 rmdir /Q /S %x86%\MultiAlignConsole\sic
@@ -56,8 +56,8 @@ del /F /S %x86%\MultiAlignConsole\*.pdb
 
 del /F /S %x64%\MultiAlignParameterFileEditor\*.vshost.exe*
 del /F /S %x64%\MultiAlignParameterFileEditor\*.pdb
-del /F /S %x64%\Manassa\*.vshost.exe*
-del /F /S %x64%\Manassa\*.pdb
+del /F /S %x64%\MultiAlign\*.vshost.exe*
+del /F /S %x64%\MultiAlign\*.pdb
 del /F /S %x64%\MultiAlignConsole\*.vshost.exe*
 del /F /S %x64%\MultiAlignConsole\*.pdb
 
