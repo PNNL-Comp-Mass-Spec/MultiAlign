6ae415f284eaf33cb693091369fc1526965a5af4
Brian LaMarche <brian.lamarche@pnnl.gov>
2014-05-07 18:26:04 +0000

    Cleaning up source trees

--- src/MultiAlignWin/EventModel/Delegates.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace MultiAlignWin
-{
-	public delegate void DelegateSetStatusMessage(int statusLevel, string status) ; 
-	public delegate void DelegateSetPercentComplete(int percentDone) ; 
-}
