eb48fdc2d5296d8335169523725ce588e1c1ad5d
alchemistmatt <matthew.monroe@pnnl.gov>
2015-04-23 19:25:03 -0700

    Add new Utilities file

---
+++ src/Test/MultiAlignTestSuite/IO/Utilities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiAlignTestSuite.IO
+{
+    class Utilities
+    {
+        public static void SleepNow(int sleepTimeSeconds)
+        {
+            var dtStartTime = DateTime.UtcNow;
+            while (DateTime.UtcNow.Subtract(dtStartTime).TotalSeconds < sleepTimeSeconds)
+            {
+                System.Threading.Thread.Sleep(25);
+            }
+        }
+    }
+}
