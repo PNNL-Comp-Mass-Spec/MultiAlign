f61293ca786ea0cf17648175ab150cf52ccf0024
Cameron Casey <cameron.casey@pnnl.gov>
2015-07-08 15:38:51 -0700

    Started working on LC-MS feature clustering.

--- src/MultiAlignRogue/MainViewModel.cs
+++ src/MultiAlignRogue/MainViewModel.cs
@@ -31,6 +31,7 @@ using MultiAlignCore.IO.Features;
 using MultiAlignCore.IO.Hibernate;
 using MultiAlignCore.IO.InputFiles;
 using PNNLOmics.Algorithms.Alignment.LcmsWarp;
+using PNNLOmics.Algorithms.Distance;
 using PNNLOmics.Algorithms.FeatureClustering;
 using PNNLOmics.Data.Features;
 
@@ -47,6 +48,7 @@ namespace MultiAlignRogue
         public MultiAlignAnalysis m_analysis { get; set;}
         VistaFolderBrowserDialog folderBrowser = new VistaFolderBrowserDialog();
         public AnalysisConfig m_config { get; set;}
+        public AlgorithmProvider m_algorithms { get; set; }
         public MultiAlignAnalysisOptions m_options { get; set;}
         public AnalysisOptionsViewModel m_AnalysisOptions { get; set;}
         private DatasetInformation m_baseline { get; set; }
@@ -55,10 +57,11 @@ namespace MultiAlignRogue
         private LCMSFeatureAligner aligner { get; set; }
 
         public ObservableCollection<DatasetInformationViewModel> Datasets { get; private set;}
-        public FeatureLoader UnalignedFeatureCache;
-        public FeatureLoader AlignedFeatureCache;
+        public FeatureLoader FeatureCache;
         public List<DatasetInformation> selectedFiles;
         public List<AlignmentType> CalibrationOptions { get; set; }
+        public List<DistanceMetric> DistanceMetrics { get; set; }
+        public List<ClusterCentroidRepresentation> CentroidRepresentations { get; set; } 
         private IFeatureWindowFactory msFeatureWindowFactory;
         private IAlignmentWindowFactory alignmentWindowFactory;
 
@@ -70,6 +73,8 @@ namespace MultiAlignRogue
         public RelayCommand AlignToBaselineCommand { get; private set; }
         public RelayCommand DisplayAlignmentCommand { get; private set; }
         public RelayCommand AddFolderCommand { get; private set; }
+        public RelayCommand ClusterFeaturesCommand { get; private set; }
+        public RelayCommand DisplayClustersCommand { get; private set; }
 
         public DataTable datasetInfo { get; set; }
         private FeatureDataAccessProviders Providers;
@@ -83,6 +88,7 @@ namespace MultiAlignRogue
         public MainViewModel()
         {
             m_config = new AnalysisConfig();
+            m_algorithms = new AlgorithmProvider();
             m_analysis = new MultiAlignAnalysis();
             m_options = m_analysis.Options;
             m_config.AnalysisName = "Analysis";
@@ -100,9 +106,10 @@ namespace MultiAlignRogue
             SearchDmsCommand = new RelayCommand(() => this.SearchDms(), () => this.ShowOpenFromDms);
             AlignToBaselineCommand = new RelayCommand(AsyncAlignToBaseline);
             DisplayAlignmentCommand = new RelayCommand(DisplayAlignment);
-            UnalignedFeatureCache = new FeatureLoader { Providers = m_analysis.DataProviders };
-            AlignedFeatureCache = new FeatureLoader { Providers = m_analysis.DataProviders };
-
+            ClusterFeaturesCommand = new RelayCommand(AsyncClusterFeatures);
+            DisplayClustersCommand = new RelayCommand(DisplayFeatures);
+            
+            FeatureCache = new FeatureLoader { Providers = m_analysis.DataProviders };
             Features = new Dictionary<DatasetInformation, IList<UMCLight>>();
             this.selectedFiles = new List<DatasetInformation>();
             msFeatureWindowFactory = new MSFeatureViewFactory();
@@ -112,6 +119,10 @@ namespace MultiAlignRogue
 
             CalibrationOptions = new List<AlignmentType>();
             Enum.GetValues(typeof(AlignmentType)).Cast<AlignmentType>().ToList().ForEach(x => CalibrationOptions.Add(x));
+            DistanceMetrics = new List<DistanceMetric>();
+            Enum.GetValues(typeof(DistanceMetric)).Cast<DistanceMetric>().ToList().ForEach(x => DistanceMetrics.Add(x));
+            CentroidRepresentations = new List<ClusterCentroidRepresentation>();
+            Enum.GetValues(typeof(ClusterCentroidRepresentation)).Cast<ClusterCentroidRepresentation>().ToList().ForEach(x => CentroidRepresentations.Add(x));
         }
         #endregion
 
@@ -220,7 +231,7 @@ namespace MultiAlignRogue
         private void LoadFeatures()
         {
             List<DatasetInformation> selectedFilesCopy = new List<DatasetInformation>();
-            UnalignedFeatureCache.Providers = m_analysis.DataProviders;
+            FeatureCache.Providers = m_analysis.DataProviders;
             foreach (var dataset in selectedFiles)
             {
                 selectedFilesCopy.Add(dataset); //Prevents crashes from changing selected files while this thread is running
@@ -232,9 +243,9 @@ namespace MultiAlignRogue
                 ThreadSafeDispatcher.Invoke(() => PlotMSFeaturesCommand.RaiseCanExecuteChanged());
                 ThreadSafeDispatcher.Invoke(() => FindMSFeaturesCommand.RaiseCanExecuteChanged());
                 IProgress<int> progress = new Progress<int>(ReportProgess);
-                var features = UnalignedFeatureCache.LoadDataset(file, m_options.MsFilteringOptions, m_options.LcmsFindingOptions,
+                var features = FeatureCache.LoadDataset(file, m_options.MsFilteringOptions, m_options.LcmsFindingOptions,
                     m_options.LcmsFilteringOptions);
-                UnalignedFeatureCache.CacheFeatures(features);
+                FeatureCache.CacheFeatures(features);
                 
                 file.FeaturesFound = true;
                 this.RaisePropertyChanged("m_analysis");
@@ -280,11 +291,11 @@ namespace MultiAlignRogue
             if (SelectedBaseline != null && SelectedBaseline.FeaturesFound)
             {
                 //Update algorithms and providers
-                AlignedFeatureCache.Providers = m_analysis.DataProviders;
+                FeatureCache.Providers = m_analysis.DataProviders;
                 algorithms = builder.GetAlgorithmProvider(m_options);
                 aligner.m_algorithms = algorithms;
 
-                var baselineFeatures = UnalignedFeatureCache.LoadDataset(m_baseline, m_options.MsFilteringOptions,
+                var baselineFeatures = FeatureCache.LoadDataset(m_baseline, m_options.MsFilteringOptions,
                     m_options.LcmsFindingOptions, m_options.LcmsFilteringOptions);
                 var alignmentData = new AlignmentDAOHibernate();
                 alignmentData.ClearAll();
@@ -298,7 +309,7 @@ namespace MultiAlignRogue
                 foreach (var file in selectedFilesCopy)
                 {
                     if (file.IsBaseline || !file.FeaturesFound) continue;
-                    var features = UnalignedFeatureCache.LoadDataset(file, m_options.MsFilteringOptions,
+                    var features = FeatureCache.LoadDataset(file, m_options.MsFilteringOptions,
                         m_options.LcmsFindingOptions, m_options.LcmsFilteringOptions);
                     var alignment = aligner.AlignToDataset(ref features, baselineFeatures, file, m_baseline);
                     //Check if there is information from a previous alignment for this dataset. If so, replace it. If not, just add the new one.
@@ -313,7 +324,7 @@ namespace MultiAlignRogue
                         AlignmentInformation.Add(alignment);
                     }
 
-                    AlignedFeatureCache.CacheFeatures(features);
+                    FeatureCache.CacheFeatures(features);
                     file.IsAligned = true;
                     this.RaisePropertyChanged("m_analysis");
                 }
@@ -578,18 +589,7 @@ namespace MultiAlignRogue
                 }
             }
         }
-        public LcmsFeatureClusteringAlgorithmType SelectedLcmsFeatureClusteringAlgorithm
-        {
-            get { return m_options.LcmsClusteringOptions.LcmsFeatureClusteringAlgorithm; }
-            set
-            {
-                if (m_options.LcmsClusteringOptions.LcmsFeatureClusteringAlgorithm != value)
-                {
-                    m_options.LcmsClusteringOptions.LcmsFeatureClusteringAlgorithm = value;
-                    this.RaisePropertyChanged("SelectedClusteringAlgorithm");
-                }
-            }
-        }
+
         public AlignmentType SelectedCalibrationType
         {
             get { return m_options.AlignmentOptions.AlignmentType; }
@@ -618,7 +618,140 @@ namespace MultiAlignRogue
             }
         }
         
-        #endregion 
+        #endregion
+
+        #region Clustering Settings
+        public LcmsFeatureClusteringAlgorithmType SelectedLcmsFeatureClusteringAlgorithm
+        {
+            get { return m_options.LcmsClusteringOptions.LcmsFeatureClusteringAlgorithm; }
+            set
+            {
+                if (m_options.LcmsClusteringOptions.LcmsFeatureClusteringAlgorithm != value)
+                {
+                    m_options.LcmsClusteringOptions.LcmsFeatureClusteringAlgorithm = value;
+                    this.RaisePropertyChanged("SelectedLcmsFeatureClusteringAlgorithm");
+                }
+            }
+        }
+
+        public ClusterCentroidRepresentation SelectedCentroidMethod
+        {
+            get { return m_options.LcmsClusteringOptions.ClusterCentroidRepresentation; }
+            set
+            {
+                if (m_options.LcmsClusteringOptions.ClusterCentroidRepresentation != value)
+                {
+                    m_options.LcmsClusteringOptions.ClusterCentroidRepresentation = value;
+                    this.RaisePropertyChanged("SelectedCentroidMethod");
+                }
+            }
+        }
+
+        public DistanceMetric SelectedDistanceFunction
+        {
+            get { return m_options.LcmsClusteringOptions.DistanceFunction; }
+            set
+            {
+                if (m_options.LcmsClusteringOptions.DistanceFunction != value)
+                {
+                    m_options.LcmsClusteringOptions.DistanceFunction = value;
+                    this.RaisePropertyChanged("SelectedDistanceFunction");
+                }
+            }
+        }
+        #endregion
+
+        #region Clustering
+
+        public async void AsyncClusterFeatures()
+        {
+            await Task.Run(() => ClusterFeatures());
+        }
+        
+        //TODO:Finish integrating clustering from MultiAlignAnalysisProcessor into Rogue
+        public void ClusterFeatures()
+        {
+            List<DatasetInformation> selectedFilesCopy = new List<DatasetInformation>();
+            foreach (var dataset in selectedFiles)
+            {
+                selectedFilesCopy.Add(dataset); //Prevents crashes from changing selected files while this thread is running
+            }
+            var clusterer = m_algorithms.Clusterer;
+            clusterer.Parameters = LcmsClusteringOptions.ConvertToOmics(m_options.LcmsClusteringOptions);
+
+            // This just tells us whether we are using mammoth memory partitions or not.          
+            var featureCache = m_analysis.DataProviders.FeatureCache;
+            var clusterCount = 0;
+
+            var providers = m_config.Analysis.DataProviders;
+
+            // Here we see if we need to separate the charge...
+            // IMS is said to require charge separation 
+            if (!m_analysis.Options.LcmsClusteringOptions.ShouldSeparateCharge)
+            {
+                var features = featureCache.FindAll();
+                var clusters = new List<UMCClusterLight>();
+                clusters = clusterer.Cluster(features, clusters);
+                foreach (var cluster in clusters)
+                {
+                    cluster.Id = clusterCount++;
+                    cluster.UmcList.ForEach(x => x.ClusterId = cluster.Id);
+
+                    // Updates the cluster with statistics
+                    foreach (var feature in cluster.UmcList)
+                    {
+                        cluster.MsMsCount += feature.MsMsCount;
+                        cluster.IdentifiedSpectraCount += feature.IdentifiedSpectraCount;
+                    }
+                }
+                providers.ClusterCache.AddAll(clusters);
+                providers.FeatureCache.UpdateAll(features);
+                m_config.Analysis.Clusters = clusters;
+            }
+            else
+            {
+                var maxChargeState = featureCache.FindMaxCharge();
+
+                /*
+                 * Here we cluster all charge states separately.  Probably IMS Data.
+                 */
+                for (var chargeState = 1; chargeState <= maxChargeState; chargeState++)
+                {
+                    var features = featureCache.FindByCharge(chargeState);
+                    if (features.Count < 1)
+                    {
+                        break;
+                    }
+
+                    var clusters = clusterer.Cluster(features);
+                    foreach (var cluster in clusters)
+                    {
+                        cluster.Id = clusterCount++;
+                        cluster.UmcList.ForEach(x => x.ClusterId = cluster.Id);
+
+                        // Updates the cluster with statistics
+                        foreach (var feature in cluster.Features)
+                        {
+                            cluster.MsMsCount += feature.MsMsCount;
+                            cluster.IdentifiedSpectraCount += feature.IdentifiedSpectraCount;
+                        }
+                    }
+
+                    m_config.Analysis.DataProviders.ClusterCache.AddAll(clusters);
+                    m_config.Analysis.DataProviders.FeatureCache.UpdateAll(features);
+                }
+                m_config.Analysis.Clusters = m_config.Analysis.DataProviders.ClusterCache.FindAll();
+            }
+
+            MessageBox.Show("Working Command");
+        }
+
+        public void DisplayFeatures()
+        {
+            MessageBox.Show("Working command");
+            //TODO: Implement
+        }
+        #endregion
 
         private void ReportProgess(int value)
         {
