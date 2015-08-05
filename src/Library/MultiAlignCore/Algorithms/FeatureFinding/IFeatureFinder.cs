28897e4171409569b156791df6ca56ef93f295ea
Cameron Casey <cameron.casey@pnnl.gov>
2015-08-05 09:18:45 -0700

    Added IFeatureFinder from PNNL-Omics to main project

---
+++ src/Library/MultiAlignCore/Algorithms/FeatureFinding/IFeatureFinder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using MultiAlignCore.Data.MetaData;
+using PNNLOmics.Algorithms;
+using PNNLOmics.Algorithms.FeatureClustering;
+using PNNLOmics.Data;
+using PNNLOmics.Data.Features;
+
+namespace MultiAlignCore.Algorithms.FeatureFinding
+{
+    /// <summary>
+    /// Creates UMC's based on MS Features.
+    /// </summary>
+    public interface IFeatureFinder: IProgressNotifer
+    {
+        /// <summary>
+        /// Finds features from the file of MS Features.
+        /// </summary>
+        List<UMCLight> FindFeatures(List<MSFeatureLight> features, 
+                                    LcmsFeatureFindingOptions   options,                                     
+                                    ISpectraProvider provider, DatasetInformation information);
+    }
+}
