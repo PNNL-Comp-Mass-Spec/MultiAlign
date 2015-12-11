c4cb52173184a7dea6e98420ede15a9d13c0ac08
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2015-12-10 22:50:18 -0800

    Modify paths for first-use scenario to user documents rather than executable path - Change the output directory to match saved project file path in first-use scenario

--- src/MultiAlignRogue/NewProjectViewModel.cs
+++ src/MultiAlignRogue/NewProjectViewModel.cs
@@ -23,6 +23,8 @@ namespace MultiAlignRogue
     /// </summary>
     public class NewProjectViewModel : ViewModelBase
     {
+        private string userDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
         /// <summary>
         /// The path to the Rogue Project file.
         /// </summary>
@@ -32,6 +34,16 @@ namespace MultiAlignRogue
         /// The path for the output (Analysis) files.
         /// </summary>
         private string outputDirectory;
+
+        /// <summary>
+        /// If the user has manually changed the output directory
+        /// </summary>
+        private bool outputDirectoryModified = false;
+
+        /// <summary>
+        /// The last stored output path
+        /// </summary>
+        private string lastOutputDirectory;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="NewProjectViewModel"/> class.
@@ -98,6 +110,29 @@ namespace MultiAlignRogue
         public string LastProjectDirectory { get; set; }
 
         /// <summary>
+        /// Last directory selected for the output files
+        /// </summary>
+        /// <remarks>This path is used by the the file selection dialog</remarks>
+        public string LastOutputDirectory {
+            get
+            {
+                return this.lastOutputDirectory;
+            }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && this.lastOutputDirectory != value)
+                {
+                    this.lastOutputDirectory = value;
+                    if (string.IsNullOrWhiteSpace(this.outputDirectory))
+                    {
+                        this.OutputDirectory = value;
+                    }
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
         /// Gets or sets the path to the Rogue Project file.
         /// </summary>
         public string ProjectFilePath
@@ -113,7 +148,7 @@ namespace MultiAlignRogue
                 this.projectFilePath = value;
                 var projectFileDirectory = Path.GetDirectoryName(value);
 
-                if (string.IsNullOrWhiteSpace(this.outputDirectory))
+                if (string.IsNullOrWhiteSpace(this.outputDirectory) || (!this.outputDirectoryModified && this.outputDirectory == this.userDocuments))
                 {
                     this.OutputDirectory = projectFileDirectory;
                 }
@@ -146,7 +181,12 @@ namespace MultiAlignRogue
                 if (string.IsNullOrWhiteSpace(this.LastProjectDirectory))
                 {
                     LastProjectDirectory = value;
-                }              
+                }
+
+                if (string.IsNullOrWhiteSpace(this.LastOutputDirectory))
+                {
+                    LastOutputDirectory = value;
+                }          
 
                 this.CreateCommand.RaiseCanExecuteChanged();
                 this.RaisePropertyChanged();
@@ -213,6 +253,8 @@ namespace MultiAlignRogue
             if (result == true)
             {
                 this.OutputDirectory = folderBrowser.SelectedPath;
+                this.outputDirectoryModified = true;
+                this.LastOutputDirectory = folderBrowser.SelectedPath;
             }
         }
 
@@ -230,6 +272,8 @@ namespace MultiAlignRogue
 
             if (!string.IsNullOrWhiteSpace(this.LastInputDirectory))
                 openFileDialog.InitialDirectory = this.LastInputDirectory;
+            else
+                openFileDialog.InitialDirectory = this.userDocuments;
 
             var result = openFileDialog.ShowDialog();
             if (result != true)
@@ -265,6 +309,7 @@ namespace MultiAlignRogue
             {
                 this.Success(this, EventArgs.Empty);
             }
+            this.LastOutputDirectory = OutputDirectory;
         }
 
         private List<DatasetInformation> GetAndValidateDatasets(IEnumerable<string> filePaths)
