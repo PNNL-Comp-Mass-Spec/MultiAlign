22ba936fe8491bad4b45c0a93559e11855c78df3
wilkinscs <wilkinscs1@gmail.com>
2016-01-19 15:21:18 -0800

    Adding converter for ascore matrix to alignment heatmap to LcmsWarpPlotDataCreator

--- src/Library/MultiAlignCore/Algorithms/Alignment/LcmsWarp/LcmsWarpPlotDataCreator.cs
+++ src/Library/MultiAlignCore/Algorithms/Alignment/LcmsWarp/LcmsWarpPlotDataCreator.cs
@@ -12,6 +12,8 @@ namespace MultiAlignCore.Algorithms.Alignment.LcmsWarp
 
     public class LcmsWarpPlotDataCreator
     {
+        private const double MinScore = -100000;
+
         /// <summary>
         /// Create histogram for normalized elution time error.
         /// </summary>
@@ -64,7 +66,7 @@ namespace MultiAlignCore.Algorithms.Alignment.LcmsWarp
             //return new MathNet.Numerics.Statistics.Histogram(driftErrors, numDriftBins);
         }
 
-        public Dictionary<double, int> GetMassErrorHistogram(List<LcmsWarpFeatureMatch> featureMatches, double massBinSize)
+        public static Dictionary<double, int> GetMassErrorHistogram(List<LcmsWarpFeatureMatch> featureMatches, double massBinSize)
         {
             var massErrors = new List<double>(featureMatches.Count);
 
@@ -85,6 +87,107 @@ namespace MultiAlignCore.Algorithms.Alignment.LcmsWarp
             //var numMassBins = Math.Max((int)Math.Floor((maxMassVal - minMassVal) / massBinSize), 1);
 
             //return new MathNet.Numerics.Statistics.Histogram(massErrors, numMassBins);
+        }
+
+        /// <summary>
+        /// Create alignment score heatmap.
+        /// The alignment score heatmap is a two dimensional matrix containing the best alignment for a
+        /// particular mapping for alignee section -> (baseline section + baseline expansion factor)
+        /// </summary>
+        /// <param name="alignmentScoreMatrix"></param>
+        /// <param name="standardize"></param>
+        /// <param name="numAligneeSections"></param>
+        /// <param name="numBaselineSections"></param>
+        /// <param name="expansionFactor"></param>
+        /// <returns></returns>
+        public static double[,] GetAlignmentHeatMap(double[,,] alignmentScoreMatrix, bool standardize, int numAligneeSections, int numBaselineSections, int expansionFactor)
+        {
+            var alignmentHeatMap = new double[numAligneeSections, numBaselineSections];
+
+            for (var aligneeSection = 0; aligneeSection < numAligneeSections; aligneeSection++)
+            {
+                for (var baselineSection = 0; baselineSection < numBaselineSections; baselineSection++)
+                {
+                    var maxScore = double.MinValue;
+                    for (var baselineSectionWidth = 0; baselineSectionWidth < expansionFactor; baselineSectionWidth++)
+                    {
+                        if (baselineSection + baselineSectionWidth >= numBaselineSections)
+                        {
+                            continue;
+                        }
+                        if (alignmentScoreMatrix[aligneeSection, baselineSection, baselineSectionWidth] > maxScore)
+                        {
+                            maxScore = alignmentScoreMatrix[aligneeSection, baselineSection, baselineSectionWidth];
+                        }
+                    }
+                    alignmentHeatMap[aligneeSection, baselineSection] = maxScore;
+                }
+            }
+
+            if (standardize)
+            {
+                StandardizeSubsectionMatchScore(alignmentHeatMap, numAligneeSections, numBaselineSections);
+            }
+
+            return alignmentHeatMap;
+        }
+
+        /// <summary>
+        /// Standardizes the match scores of the subsection
+        /// </summary>
+        /// <param name="subsectionMatchScores"></param>
+        private static void StandardizeSubsectionMatchScore(double[,] subsectionMatchScores, int numAligneeSections, int numBaselineSections)
+        {
+            for (var aligneeSection = 0; aligneeSection < numAligneeSections; aligneeSection++)
+            {
+                double sumX = 0, sumXx = 0;
+                var realMinScore = double.MaxValue;
+                var numPoints = 0;
+                for (var baselineSection = 0; baselineSection < numBaselineSections; baselineSection++)
+                {
+                    var score = subsectionMatchScores[aligneeSection, baselineSection];
+                    // if the score is greater than _minScore, basically
+                    if (Math.Abs(score - MinScore) > double.Epsilon)
+                    {
+                        realMinScore = Math.Min(realMinScore, score);
+                        sumX += score;
+                        sumXx += score * score;
+                        numPoints++;
+                    }
+                }
+                double var = 0;
+                if (numPoints > 1)
+                {
+                    var = (sumXx - ((sumX * sumX) / numPoints)) / (numPoints - 1);
+                }
+                double stDev = 1;
+                double avg = 0;
+                if (numPoints >= 1)
+                {
+                    avg = sumX / numPoints;
+                }
+                if (Math.Abs(var) > double.Epsilon)
+                {
+                    stDev = Math.Sqrt(var);
+                }
+
+                for (var baselineSection = 0; baselineSection < numBaselineSections; baselineSection++)
+                {
+                    var score = subsectionMatchScores[aligneeSection, baselineSection];
+                    if (Math.Abs(score - MinScore) < double.Epsilon)
+                    {
+                        score = realMinScore;
+                    }
+                    if (numPoints > 1)
+                    {
+                        subsectionMatchScores[aligneeSection, baselineSection] = ((score - avg) / stDev);
+                    }
+                    else
+                    {
+                        subsectionMatchScores[aligneeSection, baselineSection] = 0;
+                    }
+                }
+            }
         }
     }
 }
